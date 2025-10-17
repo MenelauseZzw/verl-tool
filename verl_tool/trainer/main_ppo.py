35675a7315772c1b62477cbf12bb8c1d87d0a042
Rex <zhongwenzrex@gmail.com>
2025-10-17 16:06:55 -0400

    add some variables to the runtime env

--- verl_tool/trainer/main_ppo.py
+++ verl_tool/trainer/main_ppo.py
@@ -37,7 +37,7 @@ def run_ppo(config) -> None:
         # NCCL debug level, VLLM logging level, and allow runtime LoRA updating
         # `num_cpus` specifies the number of CPU cores Ray can use, obtained from the configuration
         ray.init(
-            runtime_env={"env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN", "VLLM_LOGGING_LEVEL": "WARN", "VLLM_ALLOW_RUNTIME_LORA_UPDATING": "true", "VLLM_USE_V1": os.getenv("VLLM_USE_V1", "1")}},
+            runtime_env={"env_vars": {"VERL_LOGGING_LEVEL": "WARN", "TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN", "VLLM_LOGGING_LEVEL": "WARN", "VLLM_ALLOW_RUNTIME_LORA_UPDATING": "true", "VLLM_USE_V1": os.getenv("VLLM_USE_V1", "1")}},
             num_cpus=config.ray_init.num_cpus,
         )
 
